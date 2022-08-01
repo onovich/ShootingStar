d2b0a429f48734ad681ee2ccdd5d6aa72e0a7164
borui <15510964070@163.com>
2022-08-01 18:26:39 +0800

    设置修改 部分文件解除版本控制 cube测试代码

---
+++ Assets/Script/CubePlay.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlay : MonoBehaviour
+{
+    bool isOver = false;
+    void Update ()
+    {
+        if (isOver)
+        {
+            transform.localScale =
+                Vector3.Lerp (transform.localScale,Vector3.one * 1.5f, Time.deltaTime * 10f);
+        } 
+        else 
+        {
+            transform.localScale = 
+                Vector3.Lerp (transform.localScale,Vector3.one, Time.deltaTime * 5f);
+        }
+    }
+    void OnMouseEnter ()
+    {
+        isOver = true;
+    }
+    void OnMouseExit ()
+    {
+        isOver = false;
+    }
+    bool isDrag = false;
+    void OnMouseDrag ()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        Vector3 pos = Camera.main.ScreenToWorldPoint (
+            new Vector3 (mousePoint.x, mousePoint.y, 10));
+        Vector3 pos2 = Camera.main.ViewportToScreenPoint (
+            new Vector3 (mousePoint.x / Screen.width,
+                mousePoint.y / Screen.height,10f));
+        transform.position = pos;
+    }
+}
